20287dfa7ec31ed1c2bf440cf4b17d839bf52e7a
Sholpina <30983083+Sholpina@users.noreply.github.com>
2023-06-15 23:29:35 +0400

    .gitignore

--- diplom/urls.py
+++ diplom/urls.py
@@ -20,10 +20,12 @@ from django.conf import settings
 from portfolio import views
 
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
     path('blog/', include('blog.urls')),
 ]
-urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
 
